723d72056e1ca7891fc9fb6faa9b487d0449317e
John Smith <john@example.com>
2020-10-21 20:25:49 +0800

    使测试代码更简洁

--- pointer-error/wallet_test.go
+++ pointer-error/wallet_test.go
@@ -6,18 +6,21 @@ import (
 
 func TestWallet(t *testing.T) {
 
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
 		wallet.Deposit(10)
 
-		got := wallet.Balance()
+		assertBalance(t, wallet, Bitcoin(10))
 
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
@@ -25,12 +28,8 @@ func TestWallet(t *testing.T) {
 
 		wallet.Withdraw(10)
 
-		got := wallet.Balance()
-
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
+
+
 }
