fa8a501ce462cfaa07cc5b06fb41fbd217a5ba15
John Smith <john@example.com>
2020-10-21 19:36:24 +0800

    使用列表驱动测试进行一个接口的不同实现

--- structs-methods-interfaces/Perimeter.go
+++ structs-methods-interfaces/Perimeter.go
@@ -15,12 +15,22 @@ func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
+
 type Circle struct {
 	Radius float64
 }
 
 func (c Circle) Area() float64 {
 	return c.Radius * c.Radius * math.Pi
+}
+
+type Triangle struct {
+	Base float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height / 2
 }
 
 func Perimeter(rectangle Rectangle) float64 {
