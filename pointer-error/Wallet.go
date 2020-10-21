65f48043e7bb39c7bc5e116864be739fb6b8d3bc
John Smith <john@example.com>
2020-10-21 20:11:53 +0800

    由int创建新类型Bitcoin，使其具有描述性

---
+++ pointer-error/Wallet.go
@@ -0,0 +1,17 @@
+package pointer_error
+
+
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
