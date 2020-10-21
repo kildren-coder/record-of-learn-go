3db819f685f1d457d4b6d9b3a2e695efaafdbe28
John Smith <john@example.com>
2020-10-21 16:11:16 +0800

    add example and BenchmarkRepeat, make repeat can specify times in repeat

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,12 +1,33 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T)  {
-	repeated := Repeat("a")
+
+	repeated := Repeat("a", 5)
 	expected := "aaaaa"
 
 	if repeated != expected {
 		t.Errorf("expected '%q' but get '%q'", expected, repeated)
 	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+
+	var times int
+	fmt.Scan(&times)
+
+	for i := 0; i < b.N; i++ {
+		Repeat("a", times)
+	}
+}
+
+func ExampleRepeat() {
+	var temp string
+	temp = Repeat("b", 6)
+	fmt.Println(temp)
+	// Output: bbbbbb
 }
