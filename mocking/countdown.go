bd1777ff781bbe2fc1e69f52d398da84008c7a12
John Smith <john@example.com>
2020-10-25 09:42:24 +0800

    创建一个真正可用的Sleeper

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -21,7 +21,7 @@ func (s *SpySleeper) Sleep() {
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer,sleeper Sleeper) {
 	for i := countdownStart; i > 0; i -- {
 		time.Sleep(1 * time.Second)
 		fmt.Fprintln(out, i)
