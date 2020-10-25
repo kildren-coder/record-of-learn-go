c2099ccc1ce6824376fc8f9a0d59b409516bf71f
John Smith <john@example.com>
2020-10-25 11:08:27 +0800

    解决相同url问题，但这种方法可能导致多个goroutines同时写入同一个map

--- concurrency/checkwebsites.go
+++ concurrency/checkwebsites.go
@@ -10,10 +10,10 @@ func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	for _, url := range urls {
 		//对于每次迭代来说，url指向的内存地址都是一样的
 		//而在例子里，主线程的for跑完了后协程才开始跑里面的语句，导致大家的url都指向最后一个url了
-
-		go func() {
-			results[url] = wc(url)
-		}() //这个()使得该匿名函数在声明的同时执行
+		//解决方法：为每个url创建副本
+		go func(u string) {
+			results[u] = wc(u)
+		}(url) //这个()使得该匿名函数在声明的同时执行
 	}
 
 	time.Sleep(2 * time.Second)
