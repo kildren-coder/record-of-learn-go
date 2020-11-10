cc7c5930b808bff3f97839c5a79f0f68ff6e9049
kildren-coder <851283474@qq.com>
2020-11-10 09:32:36 +0800

    简陋的解决方法

--- src/web/server.go
+++ src/web/server.go
@@ -14,8 +14,13 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	player := r.URL.Path[len("/players/"):]
 
+	if r.Method == http.MethodPost {
+		w.WriteHeader(http.StatusAccepted)
+		return
+	}
+
+	player := r.URL.Path[len("/players/"):]
 	score := p.store.GetPlayerScore(player)
 
 	if score == 0 {
--- src/web/server_test.go
+++ src/web/server_test.go
@@ -74,7 +74,7 @@ func TestStoreWins(t *testing.T) {
 
 	t.Run("it returns accepted on POST", func(t *testing.T) {
 
-		request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
+		request, _ := http.NewRequest(http.MethodPost, "/players/Pepper", nil)
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
