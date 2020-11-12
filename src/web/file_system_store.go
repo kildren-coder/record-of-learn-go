6b899d276424111ddca62f089882eaa3b14e5db0
kildren-coder <851283474@qq.com>
2020-11-12 20:04:04 +0800

    通过从输入得到特定玩家分数的测试

--- src/web/file_system_store.go
+++ src/web/file_system_store.go
@@ -4,11 +4,26 @@ import (
 	"io"
 )
 
-type FileSystemStore struct {
+type FileSystemPlayerStore struct {
 	database io.ReadSeeker
 }
 
-func (f *FileSystemStore) GetLeague() []Player {
+func (f *FileSystemPlayerStore) GetLeague() []Player {
+	f.database.Seek(0, 0)
 	league, _ := NewLeague(f.database)
 	return league
+}
+
+func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
+
+	var wins int
+
+	for _, player := range f.GetLeague() {
+		if player.Name == name {
+			wins = player.Wins
+			break
+		}
+	}
+
+	return wins
 }
--- src/web/file_system_stroe_test.go
+++ src/web/file_system_stroe_test.go
@@ -12,7 +12,7 @@ func TestFileSystemStore(t *testing.T) {
 			{"Name": "Cleo", "Wins": 10},
 			{"Name": "Chris", "Wins": 33}]`)
 
-		store := FileSystemStore{database}
+		store := FileSystemPlayerStore{database}
 
 		got := store.GetLeague()
 
@@ -26,5 +26,21 @@ func TestFileSystemStore(t *testing.T) {
 		// read again
 		got = store.GetLeague()
 		assertLeague(t, got, want)
+	})
+
+	t.Run("get player score", func(t *testing.T) {
+		database := strings.NewReader(`[
+			{"Name": "Cleo", "Wins": 10},
+			{"Name": "Chris", "Wins": 33}]`)
+
+		store := FileSystemPlayerStore{database}
+
+		got := store.GetPlayerScore("Chris")
+
+		want := 33
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
 	})
 }
