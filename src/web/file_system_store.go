c96b34213864f813596d323a81b385a7b7d5606a
kildren-coder <851283474@qq.com>
2020-11-12 23:26:48 +0800

    简单重构

--- src/web/file_system_store.go
+++ src/web/file_system_store.go
@@ -11,18 +11,28 @@ type FileSystemPlayerStore struct {
 	league League
 }
 
-func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
+func initialisePlayerDBFile(file *os.File) error {
 	file.Seek(0, 0)
 
 	info, err := file.Stat()
 
 	if err != nil {
-		return nil, fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
+		return fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
 	}
 
 	if info.Size() == 0 {
 		file.Write([]byte("[]"))
-		file.Seek(0, 0)
+		file.Seek(0,0)
+	}
+
+	return nil
+}
+
+func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
+	err := initialisePlayerDBFile(file)
+
+	if err != nil {
+		return nil, fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
 	}
 
 	league, err := NewLeague(file)
