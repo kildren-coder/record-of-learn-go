851aa3dd3cfa72014d476022ad1c5067bfd2392d
kildren-coder <851283474@qq.com>
2020-11-15 14:40:27 +0800

    忘记昨晚改了啥了

--- src/web/cmd/cli/main.go
+++ src/web/cmd/cli/main.go
@@ -1,1 +1,25 @@
-package cli
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	poker "record-of-learn-go/src/web"
+)
+
+const dbFileName = "game.db.json"
+
+func main() {
+
+	store, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Let's play poker")
+	fmt.Println("Type {Name} wins to record a win")
+
+	game := poker.NewCLI(store, os.Stdin)
+	game.PlayPoker()
+}
